6128f67b2d3e1ab4cfd680f76bbd7d2c815d2586
BINHACDA115 <binhacda115@gmail.com>
2025-10-14 23:45:55 +0700

    fix: API

--- db.js
+++ db.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/yoga_blog", {
+    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/yoga_blog";
+
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
     console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
+    console.error("❌ DB error:", error.message);
     process.exit(1);
   }
 };
